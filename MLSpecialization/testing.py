aea5b25ad8085bcb8023c620dd7d126f99e69b7c
Omer-Burak-Duran <omerburakduran@gmail.com>
2024-07-01 10:27:52 +0300

    Created a new file for testing.

---
+++ MLSpecialization/testing.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.losses import BinaryCrossentropy
+
+model = Sequential([
+    Dense(units=25, activation="relu"),
+    Dense(units=15, activation="relu"),
+    Dense(units=25, activation="sigmoid")
+])
+
+model.compile(loss=BinaryCrossentropy())
+
+X = np.array()
+Y = np.array()
+
+model.fit(X,Y,epochs=100)
